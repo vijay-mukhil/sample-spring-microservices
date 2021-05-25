b9d97b9488fcb2c562d2096bc92b182357c72284
vijay-mukhil <51981601+vijay-mukhil@users.noreply.github.com>
2021-05-25 08:03:51 +0530

    Update HelloController.java

--- interaction-service/src/main/java/org/examples/java/helloworld/HelloController.java
+++ interaction-service/src/main/java/org/examples/java/helloworld/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello Vijayakumar + Spring Boot!";
+        return "Hello Jenkins X + Spring Boot!";
     }
     
 }
